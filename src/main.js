5e1d0c9400ab991c5609026f85a52001e51bbd47
rbrahul <rahulbaruri1@gmail.com>
2017-11-08 14:45:15 +0600

    Only publish required static files to show page

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue'
-import SlackComponent from './SlackComponent.vue'
-
-/* eslint-disable no-new */
-new Vue({
-  el: '#app',
-  template: '<SlackComponent/>',
-  components: { SlackComponent }
-})
